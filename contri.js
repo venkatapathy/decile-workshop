2e43b9cfd041e1b5f3ac5c8c5ce4a6ae8caa9f7c
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-03-19 23:36:08 +0530

    Created Speaker Section

--- contri.js
+++ contri.js
@@ -1,19 +1,13 @@
 var teamMember = [{
     name: 'Raja Harsh Vardhan Singh',
-    image: 'https://github.com/rhvsingh.png',
-    github: 'https://github.com/rhvsingh',
-    twitter: 'https://twitter.com/rhvsingh'
+    image: 'https://github.com/rhvsingh.png'
 }];
 
 var contributors = document.querySelectorAll('.contributors')[0];
 
 for (let i = 0; i < teamMember.length; i++) {
     let divCreate = document.createElement('div');
-    divCreate.className = 'contri_box';
-    divCreate.innerHTML = '<div class="contri-card"><div class="contri-card-inner"><div class="contri-card-front"><img src="' + teamMember[i].image + '" alt="' + teamMember[i].name + '" /></div><div class="contri-card-back"><div style="position:relative;transform-style:preserve-3d;"><span>' + teamMember[i].name + '</span><div> <li><a href="' + teamMember[i].github + '" target="_blank"><i class="fab fa-github"></i></a></li><li><a href="' + teamMember[i].twitter + '" target="_blank"><i class="fab fa-twitter"></i></a></li></div></div></div></div></div>';
+    divCreate.className = 'contri-card';
+    divCreate.innerHTML = '<div class="contri-card-front"><img src="' + teamMember[i].image + '" alt="' + teamMember[i].name + '" /></div><div class="contri-card-back"><div>' + teamMember[i].name + '</div></div>';
     contributors.append(divCreate);
 }
-
-$('.faq-heading').click(function() {
-    $(this).parent('li').toggleClass('the-active').find('.faq-text').slideToggle();
-});
