875316bf02c5141f5803371af674a7111907653a
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-03-19 23:55:17 +0530

    Added image and create condition for non-image section in speaker section

--- contri.js
+++ contri.js
@@ -1,6 +1,9 @@
 var teamMember = [{
     name: 'Raja Harsh Vardhan Singh',
     image: 'https://github.com/rhvsingh.png'
+}, {
+    name: 'Raja Harsh Vardhan Singh',
+    image: ''
 }];
 
 var contributors = document.querySelectorAll('.contributors')[0];
@@ -8,6 +11,7 @@ var contributors = document.querySelectorAll('.contributors')[0];
 for (let i = 0; i < teamMember.length; i++) {
     let divCreate = document.createElement('div');
     divCreate.className = 'contri-card';
-    divCreate.innerHTML = '<div class="contri-card-front"><img src="' + teamMember[i].image + '" alt="' + teamMember[i].name + '" /></div><div class="contri-card-back"><div>' + teamMember[i].name + '</div></div>';
+    divCreate.innerHTML = '<div class="contri-card-front"><img src="' + (teamMember[i].image != "" ? teamMember[i].image : 'images/default_pic.png') + '" alt="' + teamMember[i].name + '" /></div><div class="contri-card-back"><div>' + teamMember[i].name + '</div></div>';
+
     contributors.append(divCreate);
 }
