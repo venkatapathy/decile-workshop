8b3e4086b36a97bd85f5136e2aa2918ed1b83b7a
Shivani Shenai <shivani.shenai2000@gmail.com>
2022-03-31 09:40:57 +0530

    Updated Our Schedule

--- contri.js
+++ contri.js
@@ -1,17 +1,3 @@
-var teamMember = [{
-    name: 'Raja Harsh Vardhan Singh',
-    image: 'https://github.com/rhvsingh.png'
-}, {
-    name: 'Raja Harsh Vardhan Singh',
-    image: ''
-}];
-
-var contributors = document.querySelectorAll('.contributors')[0];
-
-for (let i = 0; i < teamMember.length; i++) {
-    let divCreate = document.createElement('div');
-    divCreate.className = 'contri-card';
-    divCreate.innerHTML = '<div class="contri-card-front"><img src="' + (teamMember[i].image != "" ? teamMember[i].image : 'images/default_pic.png') + '" alt="' + teamMember[i].name + '" /></div><div class="contri-card-back"><div>' + teamMember[i].name + '</div></div>';
-
-    contributors.append(divCreate);
-}
+$('.faq-heading').click(function() {
+    $(this).parent('li').toggleClass('the-active').find('.faq-text').slideToggle();
+});
